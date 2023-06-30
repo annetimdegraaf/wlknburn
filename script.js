e8dc4fccd944771b7bbe7a9d5403c08154b2f0e6
annetimdegraaf <annetimdegraaf@gmail.com>
2023-06-30 10:17:25 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,48 +1,48 @@
 window.addEventListener('load', function() {
-    // Fetch the burn overview data from the server-side script
-    fetch('/proxy.php')
-        .then(response => response.json())
-        .then(data => {
-            // Extract the burn overview from the data
-            const burnOverview = data.data;
+  // Fetch the burn overview data from the serverless function endpoint
+  fetch('/api/proxy')
+    .then(response => response.json())
+    .then(data => {
+      // Extract the burn overview from the data
+      const burnOverview = data.data;
 
-            // Display the burn overview on the webpage
-            const burnInfo = document.getElementById('burn-info');
-            burnInfo.innerHTML = '<h2>Burn Overview</h2>';
+      // Display the burn overview on the webpage
+      const burnInfo = document.getElementById('burn-info');
+      burnInfo.innerHTML = '<h2>Burn Overview</h2>';
 
-            const table = document.createElement('table');
-            const tableHead = document.createElement('thead');
-            const headRow = document.createElement('tr');
-            const headCell1 = document.createElement('th');
-            const headCell2 = document.createElement('th');
+      const table = document.createElement('table');
+      const tableHead = document.createElement('thead');
+      const headRow = document.createElement('tr');
+      const headCell1 = document.createElement('th');
+      const headCell2 = document.createElement('th');
 
-            headCell1.textContent = 'Transaction';
-            headCell2.textContent = 'Amount Burned';
+      headCell1.textContent = 'Transaction';
+      headCell2.textContent = 'Amount Burned';
 
-            headRow.appendChild(headCell1);
-            headRow.appendChild(headCell2);
-            tableHead.appendChild(headRow);
-            table.appendChild(tableHead);
+      headRow.appendChild(headCell1);
+      headRow.appendChild(headCell2);
+      tableHead.appendChild(headRow);
+      table.appendChild(tableHead);
 
-            const tableBody = document.createElement('tbody');
+      const tableBody = document.createElement('tbody');
 
-            burnOverview.forEach(burn => {
-                const row = document.createElement('tr');
-                const cell1 = document.createElement('td');
-                const cell2 = document.createElement('td');
+      burnOverview.forEach(burn => {
+        const row = document.createElement('tr');
+        const cell1 = document.createElement('td');
+        const cell2 = document.createElement('td');
 
-                cell1.textContent = burn.transactionHash;
-                cell2.textContent = burn.receipt.tokensBurned;
+        cell1.textContent = burn.transactionHash;
+        cell2.textContent = burn.receipt.tokensBurned;
 
-                row.appendChild(cell1);
-                row.appendChild(cell2);
-                tableBody.appendChild(row);
-            });
+        row.appendChild(cell1);
+        row.appendChild(cell2);
+        tableBody.appendChild(row);
+      });
 
-            table.appendChild(tableBody);
-            burnInfo.appendChild(table);
-        })
-        .catch(error => {
-            console.error('Error:', error);
-        });
+      table.appendChild(tableBody);
+      burnInfo.appendChild(table);
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
 });
